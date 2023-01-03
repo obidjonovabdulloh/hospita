486e65344fb500a3934047bc8cbfcbe151f2693b
muhammadaliItem <110987796+muhammadaliItem@users.noreply.github.com>
2023-01-03 19:02:47 +0500

    aliwex

--- src/pages/client/hamkorlik/index.jsx
+++ src/pages/client/hamkorlik/index.jsx
@@ -1,26 +1,23 @@
 import { useNavigate } from "react-router-dom"
-import { useState, useEffect , axios} from 'react'
-function Hamkor() {
+import { useState, useEffect } from 'react'
+import axios from 'axios';
+
+function PostPage() {
     const navigate = useNavigate();
-    const [fish, setPrice] = useState([])
-    const [email, setTitle] = useState([])
-    const [tel, setTel] = useState([])
-    const [sharx, setSharx] = useState([])
+    const [price, setPrice] = useState([])
+    const [title, setTitle] = useState([])
+    const [sale, setSale] = useState([])
     const HandleClic = (e) => {
         const kere = {
-            fish: fish,
-            email: email,
-            tel: tel,
-            sharx: sharx,
-            img: videoSelect
-
+            price: price,
+            title: title,
+            image: videoSelect,
+            sale : sale
         }
         const response = axios.post("http://localhost:8000/posts", kere)
         navigate("/")
         console.log(response);
     }
-    // 
-
     const [videoSelect, setvideoSelect] = useState()
     const [loading2, setLoading2] = useState(false)
     const [VideoData, setVideoData] = useState([])
@@ -43,7 +40,6 @@ function Hamkor() {
 
 
     }
-
     useEffect(() => {
         fetch("http://localhost:8000/posts")
             .then(res => res.json())
@@ -53,11 +49,10 @@ function Hamkor() {
     const HandleClick2 = async (e) => {
         e.preventDefault();
         const body = {
-            fish: fish,
-            email: email,
-            tel: tel,
-            sharx: sharx,
-            img: videoSelect
+            imgage: videoSelect,
+            price: price,
+            title: title,
+            sale : sale
         }
         const resPost = axios.post("http://localhost:8000/posts", body)
         window.localStorage.setItem("id", e.target.value)
@@ -65,62 +60,35 @@ function Hamkor() {
     }
     return (
         <>
-            {/* <div className="container">
-                <form className="ham_box">
-                    
-                    <input type="text" placeholder="F.I.SH" className="fish" />
-                    <input type="text" placeholder="TEL:" className="fish" />
-                    <input type="text" placeholder="Email" className="fish" />
-                    <input type="text" placeholder="Sharx" className="fish" />
-                    <button className="ham_but">Resume Yuborish</button>
-                </form>
-                
-            </div> */}
-
-
-
-
-         
-            <ul className="cont_ul">
-                <li className="cont_li">
-                   
-
-
-
-
-
-                    <form onSubmit={HandleClick2}>
+            <section className='PostImg'>
+                <div className="container">
+                    <h1 className='Addb'>Sneakers Add</h1>
+                    <div className="bags-post">
+                        <form className='for' onSubmit={HandleClick2}>
                         <input type="file" id="file" onChange={Change} />
                         <label className="custom-file-upload">
                             <span className="span-download">
                                 <ion-icon name="cloud-download-outline"></ion-icon>
                             </span>
-                           
+                            uploaded
                         </label>
                         {
                             loading2 ? (<>
                                 <span>loading</span>
                             </>) : null
                         }
-                        <div className="g">
-                            {VideoData.map((elem, index) =>
-                                <img src={elem.img} key={index} width={400} height={250} />
-                            )}
-                            <div className="ham_box">
-                                <input type="text" placeholder="F.I.SH" onChange={(e) => setPrice(e.target.value)} className="fish" />
-                                <input type="text" placeholder="TEL:" onChange={(e) => setTel(e.target.value)} className="fish" />
-                                <input type="text" placeholder="Email" onChange={(e) => setTitle(e.target.value)} className="fish" />
-                                <input type="text" placeholder="Sharx" onChange={(e) => setSharx(e.target.value)} className="fish" />
-
-                                <button className="ham_but" onClick={HandleClic} >add</button>
-                            </div>
+                        <div className="Form-box">
+                            <input type="text" onChange={(e) => setTitle(e.target.value)} placeholder="title" />
+                            <input type="text" onChange={(e) => setPrice(e.target.value)} placeholder="price" />
+                            <input type="text" onChange={(e) => setSale(e.target.value)} placeholder="sale" />
+                            <button className='btn' onClick={HandleClic} >Send</button>
                         </div>
-                        <button type="submit"> add</button>
-                    </form>
-
-                </li>
-            </ul >
+                        </form>
+                    </div>
+                </div>
+            </section>
+          
         </>
     )
 }
-export default Hamkor
+export default PostPage
