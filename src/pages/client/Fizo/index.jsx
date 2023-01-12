2da92ac4289f8c995c9d79bb926a73831d4fa1b1
muhammadaliItem <110987796+muhammadaliItem@users.noreply.github.com>
2023-01-12 20:52:54 +0500

    fvv

--- src/pages/client/Fizo/index.jsx
+++ src/pages/client/Fizo/index.jsx
@@ -2,37 +2,52 @@ import { NavLink } from "react-router-dom"
 import "./fizo.css"
 
 function Fizo() {
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     return (
         <>
             <ul className="fizo_ul">
                 <li className="fizo_li">
-                    <div className="fizo_div">
-                        <NavLink to={"/fizo1"}>
-                        <h2 className="fizoh2">Ingalatsiya</h2>
 
-                        </NavLink>
+                    <div data-aos="flip-up">
+                        <div className="fizo_div">
+                            <NavLink to={"/fizo1"}>
+                                <div>
+                                    <div className="center">
+                                        <a className="cen" href="#"><span className="cent" data-attr="Ingalatsiya"></span><span data-attr="Now"></span></a>
+                                    </div>
+                                </div>
+                            </NavLink>
+                        </div>
                     </div>
                 </li>
+
                 <li className="fizo_li">
                     <div className="fizo_div">
-                        
+
                         <NavLink to={"/fizo2"}>
-                        <h2 className="fizoh2">Igloterapiya</h2>
+                            <div className="center">
+                                <a className="cen" href="#"><span className="cent" data-attr="Ingalatsiya"></span><span data-attr="Now"></span></a>
+                            </div>
                         </NavLink>
                     </div>
                 </li>
                 <li className="fizo_li">
                     <div className="fizo_div">
-                        
+
                         <NavLink to={"/fizo3"}>
-                            <h2 className="fizoh2">Zinda</h2>
+                            <div className="center">
+                                        <a className="cen" href="#"><span className="cent" data-attr="Zinda"></span><span data-attr="Now"></span></a>
+                                    </div>
                         </NavLink>
                     </div>
                 </li>
                 <li className="fizo_li">
                     <div className="fizo_div">
-                    <NavLink to={"/fizo4"}>
-                            <h2 className="fizoh2">YBR</h2>
+                        <NavLink to={"/fizo4"}>
+                            
+                            <div className="center">
+                                        <a className="cen" href="#"><span className="cent" data-attr="_YBR_"></span><span data-attr="Now"></span></a>
+                                    </div>
                         </NavLink>
                     </div>
                 </li>
@@ -40,71 +55,91 @@ function Fizo() {
             <ul className="fizo_ul">
                 <li className="fizo_li">
                     <div className="fizo_div">
-                    <NavLink to={"/fizo5"}>
-                            <h2 className="fizoh2">Elektrofarez</h2>
+                        <NavLink to={"/fizo5"}>
+                            <div className="center">
+                                        <a className="cen" href="#"><span className="cent" data-attr="Elektrofarez"></span><span data-attr="Now"></span></a>
+                                    </div>
                         </NavLink>
                     </div>
                 </li>
                 <li className="fizo_li">
                     <div className="fizo_div">
-                    <NavLink to={"/fizo6"}>
-                            <h2 className="fizoh2">Parafen</h2>
+                        <NavLink to={"/fizo6"}>
+                        <div className="center">
+                                        <a className="cen" href="#"><span className="cent" data-attr="PARAFEN"></span><span data-attr="Now"></span></a>
+                                    </div>
                         </NavLink>
                     </div>
                 </li>
-                <li className="fizo_li"> 
-                <div className="fizo_div">
-                <NavLink to={"/fizo7"}>
-                            <h2 className="fizoh2">Lazer</h2>
-                        </NavLink>
-                </div></li>
                 <li className="fizo_li">
-                <div className="fizo_div">
-                <NavLink to={"/fizo8"}>
-                            <h2 className="fizoh2">kompda Tortish</h2>
+                    <div className="fizo_div">
+                        <NavLink to={"/fizo7"}>
+                        <div className="center">
+                                        <a className="cen" href="#"><span className="cent" data-attr="LAZER"></span><span data-attr="Now"></span></a>
+                                    </div>
+                        </NavLink>
+                    </div></li>
+                <li className="fizo_li">
+                    <div className="fizo_div">
+                        <NavLink to={"/fizo8"}>
+                        <div className="center">
+                                        <a className="cen" href="#"><span className="cent" data-attr="KOMPDA TORTISH"></span><span data-attr="Now"></span></a>
+                                    </div>
                         </NavLink>
                     </div>
                 </li>
             </ul> <ul className="fizo_ul">
                 <li className="fizo_li">
-                <div className="fizo_div">
-                <NavLink to={"/fizo9"}>
-                            <h2 className="fizoh2">Darsenval</h2>
+                    <div className="fizo_div">
+                        <NavLink to={"/fizo9"}>
+                        <div className="center">
+                                        <a className="cen" href="#"><span className="cent" data-attr="DARSENVAL"></span><span data-attr="Now"></span></a>
+                                    </div>
                         </NavLink>
                     </div>
                 </li>
                 <li className="fizo_li">
-                     <div className="fizo_div">
-                     <NavLink to={"/fizo10"}>
-                            <h2 className="fizoh2">Zuluk</h2>
+                    <div className="fizo_div">
+                        <NavLink to={"/fizo10"}>
+                        <div className="center">
+                                        <a className="cen" href="#"><span className="cent" data-attr="ZULUK"></span><span data-attr="Now"></span></a>
+                                    </div>
                         </NavLink>
                     </div>
                 </li>
                 <li className="fizo_li">
-                <div className="fizo_div">
-                <NavLink to={"/fizo11"}>
-                            <h2 className="fizoh2">AmpliPlus</h2>
+                    <div className="fizo_div">
+                        <NavLink to={"/fizo11"}>
+                        <div className="center">
+                                        <a className="cen" href="#"><span className="cent" data-attr="AMPLILPUS"></span><span data-attr="Now"></span></a>
+                                    </div>
                         </NavLink>
                     </div>
                 </li>
                 <li className="fizo_li">
-                     <div className="fizo_div">
-                     <NavLink to={"/fizo12"}>
-                            <h2 className="fizoh2">Xijama</h2>
+                    <div className="fizo_div">
+                        <NavLink to={"/fizo12"}>
+                        <div className="center">
+                                        <a className="cen" href="#"><span className="cent" data-attr="XIJAMA"></span><span data-attr="Now"></span></a>
+                                    </div>
                         </NavLink>
                     </div>
                 </li>
             </ul>
             <ul className="fizo_ul">
                 <li className="fizo_li"> <div className="fizo_div">
-                <NavLink to={"/fizo13"}>
-                            <h2 className="fizoh2">UltraVizik</h2>
-                        </NavLink>
-                    </div></li>
+                    <NavLink to={"/fizo13"}>
+                    <div className="center">
+                                        <a className="cen" href="#"><span className="cent" data-attr="ULTRA VZIK"></span><span data-attr="Now"></span></a>
+                                    </div>
+                    </NavLink>
+                </div></li>
                 <li className="fizo_li">
-                     <div className="fizo_div">
-                     <NavLink to={"/fizo14"}>
-                            <h2 className="fizoh2">ElektroUyuku</h2>
+                    <div className="fizo_div">
+                        <NavLink to={"/fizo14"}>
+                        <div className="center">
+                                        <a className="cen" href="#"><span className="cent" data-attr="ElektroUYUKA"></span><span data-attr="Now"></span></a>
+                                    </div>
                         </NavLink>
                     </div>
                 </li>
